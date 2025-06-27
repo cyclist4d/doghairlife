6019891a5bb4c37e70f17011dba2932cebeb3fbd
cyclist4d <cyclist4d@yahoo.com>
2025-06-26 20:47:21 -0400

    Update norberts_iron_dog_web_app.py

--- norberts_iron_dog_web_app.py
+++ norberts_iron_dog_web_app.py
@@ -18129,7 +18129,7 @@ with st.form("iron_dog_form"):
     ev_inches = st.number_input("Inches (Extreme Vertical)", min_value=0, max_value=11, value=0, step=1)
 
     st.subheader("Speed Retrieve")
-    sr_time = st.number_input("Time (seconds)", min_value=2.5, max_value=20.0, value=0, step=0.001, format="%.3f")
+    sr_time = st.number_input("Time (seconds)", min_value=2.5, max_value=20.0, value=2.5, step=0.001, format="%.3f")
 
     submitted = st.form_submit_button("Calculate Iron Dog Score")
 
